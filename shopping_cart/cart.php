ae3556f8cb22f9d9aed81456ae54a331f44369ac
lianne <10578051@mydbs.ie>
2022-09-04 16:13:23 +0100

    Cart added

--- shopping_cart/cart.php
+++ shopping_cart/cart.php
@@ -1,3 +1,27 @@
+<?php
+
+session_start();
+
+require_once ("php/CreateDB.php");
+require_once ("php/component.php");
+
+$db = new CreateDb(dbname:"Shopdb",tablename:"Producttb");
+
+if(isset($_POST['remove'])){
+    if ($_GET['action'] == 'remove'){
+        foreach($_SESSION['cart'] as $key => $value){
+            if($value["product_id"] == $_GET['id']){
+                unset($_SESSION['cart'][$key]);
+                echo "<script>alert('product has been removed')</script>";
+                echo "<script>window.location='cart.php'</script>";
+            }
+        }
+    }
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,39 +49,62 @@
             <h6>My Shopping Cart</h6>
             <hr>
 
-            <form action="cart.php" method="get" class="cart-items">
-                <div class="border rounded">
-                    <div class="row bg-white">
-                        <div class="col-md-3">
-                            <img src="upload/Bench.png" alt="Bench" class="img-fluid">
-                        </div>
-                        <div class="col-md-6">
-                            <h5 class="pt-2">Product1</h5>
-                            <small class="text-secondary">Seller:dailytuition</small>
-                            <h5 class="pt-2">$599</h5>
-                            <button type="submit" class="btn btn-warning">Save for Later</button>
-                            <button type="submit" class="btn btn-danger mx-2" name="remove">Remove</button>
-                        </div>
-                        <div class="col-md-3 py-5">
-                            <div>
-                                <button type="button" class="btn bg-light border rounded-circle"><i class="fas fa-minus"></i></button>
-                                <input type="text" value="1" class="form-control w-25 d-inline">
-                                <button type="button" class="btn bg-light border rounded-circle"><i class="fas fa-plus"></i></button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <?php
 
-            </div>
+            $total = 0;
+
+            if (isset($_SESSION['cart'])){
+                $product_id = array_column($_SESSION['cart'], 'product_id');
+            
+                $result = $db->getData();
+                while ($row = mysqli_fetch_assoc($result)){
+                    foreach($product_id as $id){
+                        if ($row['id'] == $id){
+                            cartElement($row['product_image'], $row['product_name'], $row['product_price'], $row['id']);
+                            $total = $total + (int)$row['product_price'];
+                        }
+                    }       
+                } 
+            }else{
+                echo"<h5>Cart is Empty</h5>";
+            }  
+
+            ?>
 
         </div>
-        <div class="col-md-5"></div>
+        <div class="col-md-4 offset-md-1 border rounded mt-5 bg-white h-25">
 
+            <div class="pt-4">
+                <h6>PRICE DETAILS</h6>
+                <hr>
+                <div class="row price-details">
+                <div class="col-md-6">
+                    <?php
+                    if (isset($_SESSION['cart'])){
+                        $count = count($_SESSION['cart']);
+                        echo "<h6>Price ($count items)</h6>";
+                    }   else{
+                        echo "<h6>Price (0 items)</h6>";
+                    }
+                    ?>
 
+                    <h6>Delivery Charges</h6>
+                    <hr>
+                    <h6>Amount Payable</h6>
+                </div>
+                <div class="col-md-6">
+                    <h6>€<?php echo $total; ?></h6>
+                    <h6 class="text-success">FREE</h6>
+                    <hr>
+                    <h6>$<?php
+                    echo $total;
+                    ?></h6>
+                </div>
+                </div>
+            </div>
+        </div>
     </div>
-
 </div>
-
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
